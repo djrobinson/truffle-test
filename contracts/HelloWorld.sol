30b42770620397c4e59f04023da8b48e634bce94
Danny Robinson <daniel@djrobinson.me>
2017-08-27 18:15:48 -0600

    Basic balance functionailty

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,15 +1,26 @@
 pragma solidity ^0.4.11;
 contract HelloWorld {
-  uint public balance;
+  address public owner;
+  mapping (address => uint) balances;
 
   // constructor
   function HelloWorld() {
-    balance = 1000;
+    owner = msg.sender;
+    balances[owner] = 1000;
   }
 
-  function deposit(uint _value) returns(uint _newValue) {
-    // ..
-    balance += _value;
-    return balance;
+  function transfer(address _to, uint _value) returns (bool success) {
+    if (balances[msg.sender] < _value) {
+      return false;
+    }
+
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    return true;
   }
+
+  function getBalance(address _user) constant returns (uint _balance) {
+    return balances[_user];
+  }
+
 }
